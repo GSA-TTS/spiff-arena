986801b317d87758c8d343b3c6bd9e9cb33fba80
Tyler Burton <tyler.burton@gsa.gov>
2026-02-24 18:48:07 -0600

    small commit to prove that we can import helpers into helpers

---
+++ spiffworkflow-backend/src/spiffworkflow_backend/scripts/process_current_user.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+from spiffworkflow_backend.models.script_attributes_context import (
+    ScriptAttributesContext,
+)
+from spiffworkflow_backend.scripts.get_current_user import GetCurrentUser
+from spiffworkflow_backend.scripts.script import Script
+
+
+class ProcessCurrentUser(Script):
+    def run(
+        self,
+        script_attributes_context: ScriptAttributesContext,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Any:
+        current_user = GetCurrentUser().run(script_attributes_context)
+        if current_user is not None:
+            return current_user
+        else:
+            return {"user": None}
