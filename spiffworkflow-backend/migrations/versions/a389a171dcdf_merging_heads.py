53c2671b2c27faad99d4f164c7859fe5a5e2c8ff
Christopher Bisom <chris.bisom@gsa.gov>
2026-04-14 10:44:02 -0400

    merge migration heads

---
+++ spiffworkflow-backend/migrations/versions/a389a171dcdf_merging_heads.py
@@ -0,0 +1,24 @@
+"""merging heads
+
+Revision ID: a389a171dcdf
+Revises: 52f3e86566bf, 8b258f77eafe
+Create Date: 2026-04-14 10:43:37.380096
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a389a171dcdf'
+down_revision = ('52f3e86566bf', '8b258f77eafe')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
